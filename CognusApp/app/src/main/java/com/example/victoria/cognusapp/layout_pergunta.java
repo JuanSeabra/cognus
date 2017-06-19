0a14f861c083775a6cab9c54f5c486678892a5e2
GabrielSaldanha <gabrielgsaldanha@gmail.com>
2017-06-19 12:26:40 -0400

    Adicionando o layout da ListView de perguntas da tela HomePage

---
+++ CognusApp/app/src/main/java/com/example/victoria/cognusapp/layout_pergunta.java
@@ -0,0 +1,13 @@
+package com.example.victoria.cognusapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout_pergunta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_pergunta);
+    }
+}
