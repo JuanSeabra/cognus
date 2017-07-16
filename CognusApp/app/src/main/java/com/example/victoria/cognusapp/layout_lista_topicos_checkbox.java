bc09e15b8ec1d39545ea91774dcb617a83e32ff6
Maria Clara <mcmelodecarvalho@gmail.com>
2017-07-16 18:43:03 -0400

    Add Layout especifico

---
+++ CognusApp/app/src/main/java/com/example/victoria/cognusapp/layout_lista_topicos_checkbox.java
@@ -0,0 +1,17 @@
+package com.example.victoria.cognusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by clara on 16/07/17.
+ */
+
+public class layout_lista_topicos_checkbox extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_lista_topicos_checkbox);
+    }
+}
